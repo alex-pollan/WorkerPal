436064a1241e63374c7b82dd8d8b617af2e2534c
alex-pollan <alexpollan@yahoo.com>
2015-12-09 21:02:44 +0000

    projects denorm unit tests

--- spec/server/projects/denormalizers-spec.js
+++ spec/server/projects/denormalizers-spec.js
@@ -1,29 +1,69 @@
 ﻿var projectDenormalizers = require('../../../server/readModels/projects/denormalizers');
 
-describe("Project denormalizers", function () {
+describe("Projects denormalizers", function () {    
     it("should handle project created", function (done) {
         //arrange
         var projectsReadModelRepository = {
             upsert: function (id, project, callback) {
                 callback(null);
             }
-        };
+        };        
+        spyOn(projectsReadModelRepository, 'upsert').andCallThrough();
+
         var sut = new projectDenormalizers.EventHandlers(projectsReadModelRepository)
-        var event = {
+        var projectCreatedEvent = {
             id: 'id',
             name : 'name',
             description: 'description',
             userId: 'userId',
-            timestamp: new Date()
+            timestamp: new Date(),
+            version: 1
         };
-
+        
         //act
-        sut.handleProjectCreated(event, function (err) {            
+        sut.handleProjectCreated(projectCreatedEvent, function (err) {
+            //assert
+            expect(projectsReadModelRepository.upsert).toHaveBeenCalled();
+            expect(projectsReadModelRepository.upsert.mostRecentCall.args[0]).toEqual(projectCreatedEvent.id);
+            var project = projectsReadModelRepository.upsert.mostRecentCall.args[1];
+            expect(project.id).toEqual(projectCreatedEvent.id);
+            expect(project.name).toEqual(projectCreatedEvent.name);
+            expect(project.description).toEqual(projectCreatedEvent.description);
+            expect(project.userId).toEqual(projectCreatedEvent.userId);
+            expect(project.created).toBe(projectCreatedEvent.timestamp);
+            expect(project.modified).toBe(projectCreatedEvent.timestamp);
+            expect(project.version).toEqual(projectCreatedEvent.version);
             done();
         });
     });
-
+    
     it("should handle project name changed", function (done) {
+        //arrange
+        var projectsReadModelRepository = {
+            update: function (id, dataToSet, callback) {
+                callback(null);
+            }
+        };
         
+        spyOn(projectsReadModelRepository, 'update').andCallThrough();
+
+        var sut = new projectDenormalizers.EventHandlers(projectsReadModelRepository)
+        var projectNameChangedEvent = {
+            id: 'id',
+            name : 'name',            
+            timestamp: new Date()
+        };
+        
+        //act
+        sut.handleProjectNameChanged(projectNameChangedEvent, function (err) {
+            //assert
+            expect(projectsReadModelRepository.update).toHaveBeenCalled();
+            expect(projectsReadModelRepository.update.mostRecentCall.args[0]).toEqual(projectNameChangedEvent.id);
+            var dataToSet = projectsReadModelRepository.update.mostRecentCall.args[1];
+            expect(dataToSet.name).toEqual(projectNameChangedEvent.name);
+            expect(dataToSet.modified).toBe(projectNameChangedEvent.timestamp);
+            expect(dataToSet.version).toEqual(projectNameChangedEvent.version);
+            done();
+        });
     });
 });
